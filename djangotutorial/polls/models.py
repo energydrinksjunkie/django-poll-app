e7663416e50344546fab3c18f62003b7c50d770b
Dragan <90929387+energydrinksjunkie@users.noreply.github.com>
2024-10-25 01:39:27 +0200

    added admin customized fields

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
@@ -11,9 +12,13 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self) -> bool:
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
